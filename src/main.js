21ad8d6eac8384f0ecf0ddafb00ddeb509375d67
カズノリ・ミヤモト <190561044@ccmailg.meijo-u.ac.jp>
2021-06-09 23:01:41 +0900

    マージ

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// import as VueGoogleMaps from 'gmap-vue'
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     key: process.env.AIzaSyCQgE9VDrIarzaPds_kwX87aus7gRxy1Ds,
+//     libraries: 'places',
+//     region: 'JP',
+//     language: 'ja'
+//   }
+// })
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
