5f0eee5817f213e793db8059319fc282f2c44fcc
jinyanomura <jinjinjinjinjin.n@gmail.com>
2021-06-12 12:56:31 +0900

    replaced tags with categories

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h1>This is {{ userId }}'s New Post Form!</h1>
-    <p>ここで新たに投稿を追加できる。</p>
+    <h1>New Post!</h1>
     <div class="submit-form">
       <div class="form-contents">
         <input type="text" v-model="postContents.title" placeholder="Title" />
@@ -10,13 +9,36 @@
           v-model="postContents.address"
           placeholder="Address"
         />
-        <input
-          type="text"
-          v-model="tag"
-          @keydown.enter="addTag"
-          placeholder="tags"
-        />
+        <select v-model="selected" @change="addCategory">
+          <option disabled>Please pick up categories</option>
+          <option>Culture</option>
+          <option>Nature</option>
+          <option>Amusement</option>
+          <option>Food</option>
+          <option>Shopping</option>
+          <option>History</option>
+          <option>Sports</option>
+          <option>View</option>
+        </select>
+        <div class="categories-holder" v-if="postContents.categories.length">
+          <span
+            v-for="(category, index) in postContents.categories"
+            :key="index"
+            @click="deleteCategory(index)"
+            >{{ category }}
+          </span>
+        </div>
         <input type="file" @change="storeImage" />
+        <div class="images-holder" v-if="postContents.imagesRef.length">
+          <div
+            class="image-container"
+            v-for="(image, index) in postContents.imagesRef"
+            :key="index"
+            @click="deleteImg(index)"
+          >
+            <img :src="image.url" />
+          </div>
+        </div>
         <textarea
           v-model="postContents.description"
           cols="30"
@@ -34,20 +56,24 @@ import firebase from "firebase";
 export default {
   data() {
     return {
-      tag: "",
+      selected: "",
       postContents: {
         title: "",
         address: "",
-        tags: [],
+        categories: [],
         imagesRef: [],
         description: "",
       },
     };
   },
   methods: {
-    addTag() {
-      this.postContents.tags.push(this.tag);
-      this.tag = "";
+    addCategory() {
+      if (!this.postContents.categories.includes(this.selected)) {
+        this.postContents.categories.push(this.selected);
+      }
+    },
+    deleteCategory(index) {
+      this.postContents.categories.splice(index, 1);
     },
     storeImage(event) {
       const file = event.target.files[0];
@@ -72,6 +98,18 @@ export default {
         })
         .catch((err) => console.log(err));
     },
+    deleteImg(index) {
+      const files = this.postContents.imagesRef;
+      firebase
+        .storage()
+        .ref(files[index].path)
+        .delete()
+        .then(() => {
+          console.log("Deleted the file");
+          files.splice(index, 1);
+        })
+        .catch((err) => console.log(err));
+    },
     submit() {
       firebase
         .firestore()
@@ -82,7 +120,10 @@ export default {
           createdAt: firebase.firestore.Timestamp.now().toDate(),
         })
         .then((res) => {
-          console.log(res.id);
+          this.$router.push({
+            name: "mypost",
+            params: { userId: this.userId, postId: res.id },
+          });
         })
         .catch((err) => console.log(err));
     },
@@ -104,5 +145,27 @@ export default {
   display: flex;
   flex-direction: column;
   width: 50%;
+}
+.categories-holder {
+  border: 1px solid black;
+  padding: 5px;
+}
+.images-holder {
+  display: flex;
+  border: 1px solid black;
+}
+.image-container {
+  width: 20%;
+  height: 100px;
+  margin: 5px;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+span {
+  border: 1px solid lightgray;
+  padding: 2px;
+  margin: 0 2px;
 }
 </style>
