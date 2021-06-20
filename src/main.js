cb6e6c00081a67f0351e15683df40a1e8769b009
jinyanomura <jinjinjinjinjin.n@gmail.com>
2021-06-20 16:23:50 +0900

    installed vue-i18n

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import "./firebase.js";
 import store from "./store/index";
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  i18n,
+  render: (h) => h(App)
 }).$mount("#app");
