d53d8d7331652f12b3adc4c9d5843764837e11ac
Kazunori-Miyamoto <190561044@ccmailg.meijo-u.ac.jp>
2021-06-24 01:24:10 +0900

    NswPost.vueにマップ追加・クリックで新たなピン建て・ウィンドウに「新規登録」の文字を反映・ピンの座標を取得

--- src/main.js
+++ src/main.js
@@ -3,9 +3,26 @@ import App from "./App.vue";
 import router from "./router";
 import "./firebase.js";
 import store from "./store/index";
+import * as VueGoogleMaps from "vue2-google-maps";
+import VueGeolocation from "vue-browser-geolocation";
+Vue.use(VueGeolocation);
 import i18n from "./i18n";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBdqpd-ViC5zdoC3XS1lOjhSNfNBcaznkw",
+    libraries: "places", // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+    region: "jp",
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+});
 
 new Vue({
   router,
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
+    <h1>This is Search Page!</h1>
+
+    <p>
+      ここにgoogle
+      mapと検索バーとか置く（ユーザーがホームの次に訪れるページのイメージ）
+    </p>
+
+    <GmapMap
+      :center="currentPotion"
+      :zoom="10"
+      map-type-id="terrain"
+      style="width: 100%; height: 600px"
+      justifiy-content-center
+      @click="getPosition($event)"
+    >
+      <GmapInfoWindow
+        :options="infoOptions"
+        :position="infoWindowPos"
+        :opened="infoWinOpen"
+        @closeclick="infoWinOpen = false"
+      >
+        {{ windowTitle }}</GmapInfoWindow
+      >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="toggleInfoWindow(m.position)"
+      />
+    </GmapMap>
+
     <select v-model="selected">
       <option value="all">{{ $t("category.all") }}</option>
       <option value="culture">{{ $t("category.culture") }}</option>
@@ -19,16 +52,95 @@
 export default {
   data() {
     return {
+      windowTitle: "hoge",
       selected: "all",
+      currentPotion: {},
+      markers: [
+        { position: { lat: 10, lng: 10 } },
+        { position: { lat: 11, lng: 11 } },
+      ],
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35,
+        },
+      },
+      infoWindowPos: null,
+      infoWinOpen: false,
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+      },
     };
   },
+  created: function () {
+    this.$getLocation().then((coordinates) => {
+      console.log(coordinates);
+      this.currentPotion = coordinates;
+      this.markers.push({
+        position: this.currentPotion,
+      });
+    });
+  },
+
   methods: {
+    onDragEnd() {
+      console.log("hoge");
+    },
+    toggleInfoWindow(position) {
+      this.infoWindowPos = position;
+      this.infoWinOpen = true;
+    },
     search() {
       this.$router.push({
         name: "result",
         params: { category: this.selected },
       });
     },
+    getPosition: function (event) {
+      console.log(event.latLng.lat());
+      if (event) {
+        this.markers.push({
+          id: this.markers.length,
+          title: "新規登録",
+          infowWindow: true,
+          position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
+        });
+      }
+      this.windowTitle = "新規登録";
+      this.toggleInfoWindow({
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      });
+    },
+  },
+  mounted: function () {
+    let origin = "41.43206,-81.38992";
+    let destination = "46.43206,-80.38992";
+    let key = "AIzaSyBdqpd-ViC5zdoC3XS1lOjhSNfNBcaznkw";
+    let url =
+      "/maps/api/directions/json?origin=" +
+      origin +
+      "&destination=" +
+      destination +
+      "&key=" +
+      key;
+    console.log(url);
+
+    fetch(url, {
+      mode: "no-cors",
+    })
+      .then((res) => {
+        console.log("status", res.status);
+        console.log("body", res.data);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
   },
 };
 </script>
