5f3b0b81df9d78cd07e775b4cd0fd4db062561b8
jinyanomura <jinjinjinjinjin.n@gmail.com>
2021-06-25 23:19:31 +0900

    form.vue not done ver.2

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,26 +1,7 @@
 <template>
-  <!-- <b-container>
+  <b-container>
     <b-form-row>
       <b-form @submit.prevent="send">
-        <b-form-group :label="`${$t('form.image')} :`" label-for="images">
-          <b-form-file
-            id="images"
-            accept=".jpg, .jpeg, .png"
-            placeholder="Choose a file or drop it here..."
-            @change.self="storeImage"
-            required
-          ></b-form-file>
-        </b-form-group>
-        <div v-if="postContents.imagesRef.length">
-          <div
-            v-for="(image, index) in postContents.imagesRef"
-            :key="index"
-            @click="deleteImage(index)"
-          >
-            <img :src="image.url" />
-          </div>
-        </div>
-
         <b-form-group :label="`${$t('form.title')} :`" label-for="title">
           <b-form-input
             id="title"
@@ -61,13 +42,28 @@
           ></b-form-textarea>
         </b-form-group>
 
+        <b-form-group :label="`${$t('form.image')} :`" label-for="images">
+          <b-form-file
+            id="images"
+            accept=".jpg, .jpeg, .png"
+            placeholder="Choose a file or drop it here..."
+            @change.self="storeImage"
+            required
+          ></b-form-file>
+        </b-form-group>
+        <div v-if="postContents.imagesRef.length">
+          <div
+            v-for="(image, index) in postContents.imagesRef"
+            :key="index"
+            @click="deleteImage(index)"
+          >
+            <img :src="image.url" />
+          </div>
+        </div>
+
         <div v-if="$route.name === 'newpost'">
-          <b-button type="submit" name="action" value="submit">{{
-            $t("form.submit")
-          }}</b-button>
-          <b-button type="submit" name="action" value="save">{{
-            $t("form.save")
-          }}</b-button>
+          <b-button type="submit">{{ $t("form.submit") }}</b-button>
+          <b-button type="submit">{{ $t("form.save") }}</b-button>
         </div>
         <div v-if="$route.name === 'edit'">
           <b-button type="submit" name="action" value="update">{{
@@ -76,50 +72,7 @@
         </div>
       </b-form>
     </b-form-row>
-  </b-container> -->
-
-  <div class="form-contents">
-    <input
-      type="text"
-      v-model="postContents.title"
-      :placeholder="$t('form.title')"
-    />
-    <select v-model="selected" @change="addCategory">
-      <option value="culture">{{ $t("category.culture") }}</option>
-      <option value="nature">{{ $t("category.nature") }}</option>
-      <option value="amusement">{{ $t("category.amusement") }}</option>
-      <option value="food">{{ $t("category.food") }}</option>
-      <option value="shopping">{{ $t("category.shopping") }}</option>
-      <option value="history">{{ $t("category.history") }}</option>
-      <option value="sports">{{ $t("category.sports") }}</option>
-      <option value="view">{{ $t("category.view") }}</option>
-    </select>
-    <div class="categories-holder" v-if="postContents.categories.length">
-      <span
-        v-for="(category, index) in postContents.categories"
-        :key="index"
-        @click="deleteCategory(index)"
-        >{{ category }}
-      </span>
-    </div>
-    <input type="file" accept=".jpg, .jpeg, .png" @change="storeImage" />
-    <div class="images-holder" v-if="postContents.imagesRef.length">
-      <div
-        class="image-container"
-        v-for="(image, index) in postContents.imagesRef"
-        :key="index"
-        @click="deleteImage(index)"
-      >
-        <img :src="image.url" />
-      </div>
-    </div>
-    <textarea
-      v-model="postContents.description"
-      cols="30"
-      rows="10"
-      :placeholder="$t('form.description')"
-    ></textarea>
-  </div>
+  </b-container>
 </template>
 
 <script>
