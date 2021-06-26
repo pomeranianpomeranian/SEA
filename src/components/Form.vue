d6545122b1f8eac76756f483cdfcec8eb7b2b4c5
jinyanomura <jinjinjinjinjin.n@gmail.com>
2021-06-26 13:13:17 +0900

    transfered map from Form.vue to NewPost.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container>
     <b-form-row>
-      <b-form class="col-8 offset-2">
+      <b-form class="col">
         <b-form-group :label="`${$t('form.title')} :`" label-for="title">
           <b-form-input
             id="title"
@@ -80,53 +80,6 @@
           </b-row>
         </b-container>
 
-        <p class="mt-3 mb-2">{{ $t("form.location") }}</p>
-        <GmapMap
-          class=""
-          :center="currentPosition"
-          :zoom="10"
-          map-type-id="terrain"
-          style="width: 100%; height: 600px"
-          @click="getPosition($event)"
-        >
-          <GmapInfoWindow
-            :options="infoOptions"
-            :position="postContents.position"
-            :opened="infoWinOpen"
-            @closeclick="infoWinOpen = false"
-          >
-            {{ windowTitle }}</GmapInfoWindow
-          >
-          <GmapMarker
-            :key="index"
-            v-for="(m, index) in markers"
-            :position="m.position"
-            :clickable="true"
-            :draggable="true"
-            @click="toggleInfoWindow(m.position)"
-          />
-        </GmapMap>
-
-        <div class="mt-3" v-if="$route.name === 'newpost'">
-          <b-button
-            class="col-6"
-            variant="outline-info"
-            type="button"
-            size="lg"
-            @click="submitPost(false)"
-            v-bind="{ disabled: !completed }"
-            >{{ $t("form.submit") }}</b-button
-          >
-          <b-button
-            class="col-6"
-            variant="outline-dark"
-            type="button"
-            size="lg"
-            @click="submitPost(true)"
-            v-bind="{ disabled: !completed }"
-            >{{ $t("form.save") }}</b-button
-          >
-        </div>
         <div class="mt-3" v-if="$route.name === 'edit'">
           <b-button
             class="col"
@@ -148,7 +101,6 @@ import { mapActions } from "vuex";
 export default {
   data() {
     return {
-      windowTitle: "hoge",
       selected: "",
       categories: [
         { value: "", text: this.$t("placeholder.category"), disabled: true },
@@ -161,20 +113,10 @@ export default {
         { value: "sports", text: this.$t("category.sports") },
         { value: "view", text: this.$t("category.view") },
       ],
-      currentPosition: {},
-      markers: [],
-      infoOptions: {
-        pixelOffset: {
-          width: 0,
-          height: -35,
-        },
-      },
-      infoWindowPos: null,
-      infoWinOpen: false,
     };
   },
   methods: {
-    ...mapActions(["deleteImage", "storeImage", "submitPost", "updatePost"]),
+    ...mapActions(["deleteImage", "storeImage", "updatePost"]),
     addCategory() {
       if (!this.postContents.categories.includes(this.selected)) {
         this.postContents.categories.push(this.selected);
@@ -183,52 +125,10 @@ export default {
     deleteCategory(index) {
       this.postContents.categories.splice(index, 1);
     },
-    toggleInfoWindow(position) {
-      this.postContents.position = position;
-      this.infoWinOpen = true;
-    },
-    getPosition: function (event) {
-      if (this.markers.length >= 1) {
-        this.markers.splice(1);
-      }
-      if (event) {
-        this.markers.push({
-          id: this.markers.length,
-          title: this.postContents.title,
-          infowWindow: true,
-          position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
-        });
-      }
-      this.windowTitle = this.postContents.title;
-      this.toggleInfoWindow({
-        lat: event.latLng.lat(),
-        lng: event.latLng.lng(),
-      });
-    },
-  },
-  created() {
-    this.$getLocation().then((coordinates) => {
-      this.currentPosition = coordinates;
-      this.markers.push({
-        position: this.currentPosition,
-      });
-    });
-    this.$store.commit("clearContents");
   },
   computed: {
     postContents() {
       return this.$store.state.post.postContents;
-    },
-    completed() {
-      if (
-        this.postContents.title &&
-        this.postContents.description &&
-        this.postContents.categories.length &&
-        this.postContents.imagesRef.length &&
-        this.postContents.position
-      )
-        return true;
-      else return false;
     },
   },
 };
